e0ec1bf50c162aeb965e813f4b923c6083ef9466
seeo <aragonlotr5@gmail.com>
2019-05-02 18:58:20 +0800

    trying to play around with the development.rb file: config.assets.debug = false and  config.assets.digest = false. not sure why I am only generating one single application.js. Am expecting to generate muultiple js files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,10 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+
+  #When this option is true, digests will be generated for asset URLs. Put this option in to see what happens.
+  config.assets.digest = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
